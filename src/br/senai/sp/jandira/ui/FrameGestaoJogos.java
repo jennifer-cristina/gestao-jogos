b18aabe53be9a41d688269352c50c529859cc4a5
JENNIFER CRISTINA SANTOS DA SILVA <21276662@CFP127ALUNO.LOCAL>
2021-11-25 16:56:35 -0300

    Finalizado

--- src/br/senai/sp/jandira/ui/FrameGestaoJogos.java
+++ src/br/senai/sp/jandira/ui/FrameGestaoJogos.java
@@ -15,6 +15,7 @@ import javax.swing.event.ListSelectionListener;
 import br.senai.sp.jandira.model.Console;
 import br.senai.sp.jandira.model.Fabricante;
 import br.senai.sp.jandira.model.Jogo;
+import br.senai.sp.jandira.model.Numeros;
 import br.senai.sp.jandira.repository.FabricanteRepository;
 import br.senai.sp.jandira.repository.JogoRepository;
 
@@ -41,6 +42,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JTabbedPane;
 import javax.swing.JSlider;
 import java.awt.Font;
+import java.awt.Color;
 
 public class FrameGestaoJogos extends JFrame {
 
@@ -59,21 +61,17 @@ public class FrameGestaoJogos extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 533, 392);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
 		contentPane.setToolTipText("");
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JPanel panelFormularioJogos = new JPanel();
-		panelFormularioJogos.setBounds(59, 0, 458, 361);
+		panelFormularioJogos.setBounds(59, 0, 458, 353);
 		contentPane.add(panelFormularioJogos);
 		panelFormularioJogos.setLayout(null);
 		panelFormularioJogos.setVisible(true);
-
-		JPanel panelFormularioFabricante = new JPanel();
-		panelFormularioFabricante.setBounds(59, 0, 466, 361);
-		contentPane.add(panelFormularioFabricante);
-		panelFormularioFabricante.setVisible(false);
 
 		JLabel lblTitulo = new JLabel("Titulo:");
 		lblTitulo.setFont(new Font("Arial", Font.BOLD, 14));
@@ -81,7 +79,7 @@ public class FrameGestaoJogos extends JFrame {
 		panelFormularioJogos.add(lblTitulo);
 
 		txtTitulo = new JTextField();
-		txtTitulo.setBounds(26, 90, 86, 20);
+		txtTitulo.setBounds(26, 88, 86, 20);
 		panelFormularioJogos.add(txtTitulo);
 		txtTitulo.setColumns(10);
 
@@ -93,49 +91,48 @@ public class FrameGestaoJogos extends JFrame {
 		JComboBox cmbFabricante = new JComboBox();
 		cmbFabricante.setBounds(26, 149, 86, 22);
 		panelFormularioJogos.add(cmbFabricante);
-
-		JogoRepository jogos = new JogoRepository(32);
+		
 		FabricanteRepository fabricantes = new FabricanteRepository(32);
 
 		DefaultComboBoxModel<String> modelFabricante = carregarFabricantes(fabricantes);
-
+		
 		cmbFabricante.setModel(modelFabricante);
 
 		JCheckBox chbZerado = new JCheckBox("Zerado");
 		chbZerado.setFont(new Font("Arial", Font.BOLD, 14));
 		chbZerado.setHorizontalAlignment(SwingConstants.CENTER);
-		chbZerado.setBounds(156, 181, 86, 23);
+		chbZerado.setBounds(156, 191, 86, 23);
 		panelFormularioJogos.add(chbZerado);
 
 		JLabel lblConsole = new JLabel("Console:");
 		lblConsole.setFont(new Font("Arial", Font.BOLD, 14));
-		lblConsole.setBounds(156, 63, 73, 14);
+		lblConsole.setBounds(156, 124, 73, 14);
 		panelFormularioJogos.add(lblConsole);
 
 		JLabel lblValor = new JLabel("Valor:");
 		lblValor.setFont(new Font("Arial", Font.BOLD, 14));
-		lblValor.setBounds(156, 124, 52, 14);
+		lblValor.setBounds(156, 62, 52, 14);
 		panelFormularioJogos.add(lblValor);
 
 		txtValor = new JTextField();
 		txtValor.setColumns(10);
-		txtValor.setBounds(156, 150, 86, 20);
+		txtValor.setBounds(156, 88, 86, 20);
 		panelFormularioJogos.add(txtValor);
+		
+		txtValor.setDocument(new Numeros());
 
 		JLabel lblObservacoes = new JLabel("Observa\u00E7\u00F5es:");
 		lblObservacoes.setFont(new Font("Arial", Font.BOLD, 14));
-		lblObservacoes.setBounds(26, 190, 104, 14);
+		lblObservacoes.setBounds(26, 195, 104, 14);
 		panelFormularioJogos.add(lblObservacoes);
 
 		JComboBox cmbConsole = new JComboBox();
-		cmbConsole.setBounds(156, 89, 86, 22);
+		cmbConsole.setBounds(156, 150, 86, 22);
 		panelFormularioJogos.add(cmbConsole);
+
+
 		DefaultComboBoxModel<String> modelConsole = new DefaultComboBoxModel<String>();
-
-		for (Console c : Console.values()) {
-			modelConsole.addElement(c.getDescricao());
-		}
-
+		
 		cmbConsole.setModel(modelConsole);
 
 		JLabel lblMeusJogos = new JLabel("Meus Jogos:");
@@ -144,78 +141,80 @@ public class FrameGestaoJogos extends JFrame {
 		panelFormularioJogos.add(lblMeusJogos);
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(301, 91, 132, 203);
+		scrollPane.setBounds(284, 90, 132, 203);
 		panelFormularioJogos.add(scrollPane);
+
+		JList listJogos = new JList();
+		scrollPane.setViewportView(listJogos);
 		
-				JList listJogos = new JList();
-				scrollPane.setViewportView(listJogos);
-				
-						listJogos.addKeyListener(new KeyListener() {
-				
-							@Override
-							public void keyReleased(KeyEvent key) {
-				
-								int code = key.getKeyCode();
-				
-								if (code == KeyEvent.VK_RIGHT) {
-									listJogos.setSelectedIndex(listJogos.getSelectedIndex() + 1);
-								} else if (code == KeyEvent.VK_LEFT) {
-									listJogos.setSelectedIndex(listJogos.getSelectedIndex() - 1);
-								}
-				
-							}
-				
-							@Override
-							public void keyTyped(KeyEvent key) {
-							}
-				
-							@Override
-							public void keyPressed(KeyEvent e) {
-				
-							}
-				
-						});
-						
-						DefaultListModel<String> listaModel = new DefaultListModel<String>();
+		JogoRepository jogos = new JogoRepository(32);
 
-								listJogos.setModel(listaModel);
-								
-										listJogos.addListSelectionListener(new ListSelectionListener() {
-								
-											@Override
-											public void valueChanged(ListSelectionEvent e) {
-								
-												Jogo jogo = jogos.listarJogo(listJogos.getSelectedIndex());
-												txtTitulo.setText(jogo.getTitulo());
-								
-											}
-										});
-										
-												listJogos.addListSelectionListener(new ListSelectionListener() {
-										
-													@Override
-													public void valueChanged(ListSelectionEvent e) {
-										
-														Jogo jogo = jogos.listarJogo(listJogos.getSelectedIndex());
-										
-														txtTitulo.setText(jogo.getTitulo());
-														cmbFabricante.setSelectedIndex(fabricantes.getIndex(jogo.getFabricante()));
-														chbZerado.isSelected();
-														cmbConsole.setSelectedIndex(jogo.getConsole().ordinal());
-														txtValor.setText(String.valueOf(jogo.getValor()));
-														txtObservacoes.setText(jogo.getObservacoes());
-										
-													}
-												});
 
+		listJogos.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyReleased(KeyEvent key) {
+
+				int code = key.getKeyCode();
+
+				if (code == KeyEvent.VK_RIGHT) {
+					listJogos.setSelectedIndex(listJogos.getSelectedIndex() + 1);
+				} else if (code == KeyEvent.VK_LEFT) {
+					listJogos.setSelectedIndex(listJogos.getSelectedIndex() - 1);
+				}
+
+			}
+
+			@Override
+			public void keyTyped(KeyEvent key) {
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+
+			}
+
+		});
+		
+		DefaultListModel<String> listaModel = new DefaultListModel<String>();
+
+		listJogos.setModel(listaModel);
+
+		listJogos.addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+
+				Jogo jogo = jogos.listarJogo(listJogos.getSelectedIndex());
+				txtTitulo.setText(jogo.getTitulo());
+
+			}
+		});
+
+		listJogos.addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+
+				Jogo jogo = jogos.listarJogo(listJogos.getSelectedIndex());
+
+				txtTitulo.setText(jogo.getTitulo());
+				cmbFabricante.setSelectedIndex(fabricantes.getIndex(jogo.getFabricante()));
+				chbZerado.setSelected(jogo.isZerado());;
+				cmbConsole.setSelectedIndex(jogo.getConsole().ordinal());
+				txtValor.setText(String.valueOf(jogo.getValor()));
+				txtObservacoes.setText(jogo.getObservacoes());
+
+			}
+		});
 
 		JButton btnEsquerda = new JButton("<");
 		btnEsquerda.setToolTipText("<");
-		btnEsquerda.setBounds(327, 312, 41, 23);
+		btnEsquerda.setBounds(294, 304, 41, 23);
 		panelFormularioJogos.add(btnEsquerda);
 
 		JButton btnDireita = new JButton(">");
-		btnDireita.setBounds(378, 312, 41, 23);
+		btnDireita.setBounds(364, 304, 41, 23);
 		panelFormularioJogos.add(btnDireita);
 
 		btnEsquerda.addActionListener(new ActionListener() {
@@ -238,15 +237,9 @@ public class FrameGestaoJogos extends JFrame {
 			}
 		});
 
-		getRootPane().getActionMap().put("myAction", new AbstractAction() {
-			public void actionPerformed(java.awt.event.ActionEvent e) {
-				System.out.println("Funfou");
-			}
-		});
-
 		JButton btnSalvar = new JButton("Salvar Jogo");
 		btnSalvar.setFont(new Font("Arial", Font.BOLD, 14));
-		btnSalvar.setBounds(156, 311, 140, 23);
+		btnSalvar.setBounds(68, 287, 140, 48);
 		panelFormularioJogos.add(btnSalvar);
 
 		JLabel lblTituloFormularioJogo = new JLabel("Formul\u00E1rio de Jogos");
@@ -255,13 +248,9 @@ public class FrameGestaoJogos extends JFrame {
 		panelFormularioJogos.add(lblTituloFormularioJogo);
 
 		txtObservacoes = new JTextField();
-		txtObservacoes.setBounds(26, 220, 105, 94);
+		txtObservacoes.setBounds(26, 220, 216, 56);
 		panelFormularioJogos.add(txtObservacoes);
 		txtObservacoes.setColumns(10);
-
-		JButton btnSalvarFabricante = new JButton("Salvar");
-		btnSalvarFabricante.setBounds(169, 72, 89, 23);
-		panelFormularioFabricante.add(btnSalvarFabricante);
 
 		btnSalvar.addActionListener(new ActionListener() {
 
@@ -283,9 +272,25 @@ public class FrameGestaoJogos extends JFrame {
 				posicaoJogo++;
 
 				listaModel.addElement(jogo.getTitulo());
+				
+				txtTitulo.setText("");
+				txtValor.setText("");
+				txtObservacoes.setText("");
+				chbZerado.setSelected(false);
 
 			}
 		});
+
+		JPanel panelFormularioFabricante = new JPanel();
+		panelFormularioFabricante.setBounds(59, 0, 466, 361);
+		contentPane.add(panelFormularioFabricante);
+		panelFormularioFabricante.setVisible(false);
+		panelFormularioFabricante.setLayout(null);
+
+		JButton btnSalvarFabricante = new JButton("Salvar");
+		btnSalvarFabricante.setFont(new Font("Arial", Font.BOLD, 15));
+		btnSalvarFabricante.setBounds(255, 77, 86, 20);
+		panelFormularioFabricante.add(btnSalvarFabricante);
 
 		btnSalvarFabricante.addActionListener(new ActionListener() {
 
@@ -303,16 +308,39 @@ public class FrameGestaoJogos extends JFrame {
 		});
 
 		JLabel lblNome = new JLabel("Nome:");
-		lblNome.setBounds(169, 33, 46, 14);
+		lblNome.setFont(new Font("Arial", Font.BOLD, 15));
+		lblNome.setBounds(109, 75, 86, 20);
 		panelFormularioFabricante.add(lblNome);
 
 		txtNome = new JTextField();
-		txtNome.setBounds(211, 30, 86, 20);
+		txtNome.setBounds(166, 77, 86, 20);
 		panelFormularioFabricante.add(txtNome);
 		txtNome.setColumns(10);
 
+		JLabel lblFormularioFabricante = new JLabel("Formulario de Fabricantes");
+		lblFormularioFabricante.setFont(new Font("Arial", Font.BOLD, 20));
+		lblFormularioFabricante.setBounds(99, 23, 256, 41);
+		panelFormularioFabricante.add(lblFormularioFabricante);
+
+
+
+
+
+		for (Console c : Console.values()) {
+			modelConsole.addElement(c.getDescricao());
+		}
+
+
+
+		getRootPane().getActionMap().put("myAction", new AbstractAction() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+				System.out.println("Funfou");
+			}
+		});
+
 		JPanel panelMenu = new JPanel();
-		panelMenu.setBounds(10, 57, 50, 304);
+		panelMenu.setBackground(Color.LIGHT_GRAY);
+		panelMenu.setBounds(8, 60, 50, 99);
 		contentPane.add(panelMenu);
 
 		JButton btnFormularioJogo = new JButton();
